beab3cec6b6c04cdbd50727e554852a4d05c1572
Michael Harrington <mdharr7@gmail.com>
2022-12-22 05:39:53 +0800

    Create Boot/MVC project, connect JPA dependency

---
+++ OpenOffice/src/test/java/com/skilldistillery/jobsearch/OpenOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
