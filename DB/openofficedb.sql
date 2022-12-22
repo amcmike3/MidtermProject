30840069aeae38bbf0dcef1585b2d28ef674e463
Amcmike3 <amcmichael3@gmail.com>
2022-12-22 10:51:51 -0700

    update openofficedb.sql script, add new tables to schema, add foreign key relationships to tables

--- DB/openofficedb.sql
+++ DB/openofficedb.sql
@@ -16,6 +16,20 @@ CREATE SCHEMA IF NOT EXISTS `open_officedb` DEFAULT CHARACTER SET utf8 ;
 USE `open_officedb` ;
 
 -- -----------------------------------------------------
+-- Table `company_image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `company_image` ;
+
+CREATE TABLE IF NOT EXISTS `company_image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `img_url` VARCHAR(2000) NOT NULL,
+  `date_created` DATETIME NULL,
+  `img_description` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `company`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `company` ;
@@ -24,7 +38,14 @@ CREATE TABLE IF NOT EXISTS `company` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NULL,
   `location` VARCHAR(500) NULL,
-  PRIMARY KEY (`id`))
+  `company_image_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_company_company_image1_idx` (`company_image_id` ASC),
+  CONSTRAINT `fk_company_company_image1`
+    FOREIGN KEY (`company_image_id`)
+    REFERENCES `company_image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -50,7 +71,15 @@ CREATE TABLE IF NOT EXISTS `benefit` (
   `home_office_stipend` TINYINT NOT NULL DEFAULT 0,
   `education_benefits` TINYINT NOT NULL DEFAULT 0,
   `stock_options` TINYINT NULL DEFAULT 0,
-  PRIMARY KEY (`id`))
+  `company_id` INT NOT NULL,
+  `other` VARCHAR(500) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_benefit_company1_idx` (`company_id` ASC),
+  CONSTRAINT `fk_benefit_company1`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `company` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -62,6 +91,23 @@ DROP TABLE IF EXISTS `job` ;
 CREATE TABLE IF NOT EXISTS `job` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `salary` INT NULL,
+  `years_experience` INT NULL,
+  `skills` TEXT NULL,
+  `education` VARCHAR(100) NULL,
+  `certifications` VARCHAR(200) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_image`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_image` ;
+
+CREATE TABLE IF NOT EXISTS `user_image` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `img_url` VARCHAR(2000) NOT NULL,
+  `date_created` DATETIME NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -73,20 +119,124 @@ DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `experience` TEXT NULL,
-  `skills` TEXT NULL,
-  `education` TEXT NULL,
-  `pic` VARCHAR(45) NULL,
-  `certifications` TEXT NULL,
-  `veteran` TINYINT NOT NULL DEFAULT 0,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   `enabled` TINYINT NULL DEFAULT 1,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `role` VARCHAR(45) NULL,
+  `first_name` VARCHAR(45) NULL DEFAULT 'Johnny',
+  `last_name` VARCHAR(45) NULL DEFAULT 'Doughboy',
+  `role` VARCHAR(45) NULL DEFAULT 'USER',
+  `job_id` INT NOT NULL,
+  `user_image_id` INT NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_user_job_idx` (`job_id` ASC),
+  INDEX `fk_user_user_image1_idx` (`user_image_id` ASC),
+  CONSTRAINT `fk_user_job`
+    FOREIGN KEY (`job_id`)
+    REFERENCES `job` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_user_image1`
+    FOREIGN KEY (`user_image_id`)
+    REFERENCES `user_image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `company_jobs`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `company_jobs` ;
+
+CREATE TABLE IF NOT EXISTS `company_jobs` (
+  `job_id` INT NOT NULL,
+  `company_id` INT NOT NULL,
+  PRIMARY KEY (`job_id`, `company_id`),
+  INDEX `fk_job_has_company_company1_idx` (`company_id` ASC),
+  INDEX `fk_job_has_company_job1_idx` (`job_id` ASC),
+  CONSTRAINT `fk_job_has_company_job1`
+    FOREIGN KEY (`job_id`)
+    REFERENCES `job` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_job_has_company_company1`
+    FOREIGN KEY (`company_id`)
+    REFERENCES `company` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `interview`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `interview` ;
+
+CREATE TABLE IF NOT EXISTS `interview` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `process` TEXT NOT NULL,
+  `comment_date` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  `company_jobs_job_id` INT NOT NULL,
+  `company_jobs_company_id` INT NOT NULL,
+  `title` VARCHAR(45) NULL,
+  `job_offered` TINYINT NOT NULL,
+  `questions` TEXT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_company_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_company_comment_company_jobs1_idx` (`company_jobs_job_id` ASC, `company_jobs_company_id` ASC),
+  CONSTRAINT `fk_company_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_company_comment_company_jobs1`
+    FOREIGN KEY (`company_jobs_job_id` , `company_jobs_company_id`)
+    REFERENCES `company_jobs` (`job_id` , `company_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `company_review`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `company_review` ;
+
+CREATE TABLE IF NOT EXISTS `company_review` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `content` TEXT NOT NULL,
+  `review_date` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  `company_jobs_job_id` INT NOT NULL,
+  `company_jobs_company_id` INT NOT NULL,
+  `benefit_id` INT NOT NULL,
+  `recommendation` TINYINT NOT NULL,
+  `rating` INT NOT NULL,
+  `pros` TEXT NULL,
+  `cons` TEXT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `advice` TEXT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_company_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_company_comment_company_jobs1_idx` (`company_jobs_job_id` ASC, `company_jobs_company_id` ASC),
+  INDEX `fk_company_review_benefit1_idx` (`benefit_id` ASC),
+  CONSTRAINT `fk_company_comment_user10`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_company_comment_company_jobs10`
+    FOREIGN KEY (`company_jobs_job_id` , `company_jobs_company_id`)
+    REFERENCES `company_jobs` (`job_id` , `company_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_company_review_benefit1`
+    FOREIGN KEY (`benefit_id`)
+    REFERENCES `benefit` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -101,11 +251,21 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `job`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `open_officedb`;
+INSERT INTO `job` (`id`, `salary`, `years_experience`, `skills`, `education`, `certifications`) VALUES (1, 89999, 25, 'Administration', 'Doctorates in website administration', 'Expert Administration from this institute of administering administration');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `open_officedb`;
-INSERT INTO `user` (`id`, `experience`, `skills`, `education`, `pic`, `certifications`, `veteran`, `username`, `password`, `enabled`, `first_name`, `last_name`, `role`) VALUES (1, NULL, NULL, NULL, NULL, NULL, DEFAULT, 'openadmin', 'openadmin', 1, 'open', 'admin', 'admin');
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `first_name`, `last_name`, `role`, `job_id`, `user_image_id`) VALUES (1, 'openadmin', 'openadmin', 1, 'open', 'admin', 'admin', 1, NULL);
 
 COMMIT;
 
