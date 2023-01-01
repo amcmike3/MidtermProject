0c0d4ad6df0608ef87abce826d3691d86e29b53e
Amcmike3 <amcmichael3@gmail.com>
2023-01-01 15:45:34 -0700

    fix UserDaoImpl methods isEmailUnique and isUsernameUnique so that they will return true if the passed in info is unique and false if it is not unique

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/UserDaoImpl.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/UserDaoImpl.java
@@ -1,6 +1,9 @@
 package com.skilldistillery.jobsearch.data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
@@ -11,10 +14,10 @@ import com.skilldistillery.jobsearch.entities.User;
 @Service
 @Transactional
 public class UserDaoImpl implements UserDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public User findById(int userId) {
 		return em.find(User.class, userId);
@@ -30,7 +33,7 @@ public class UserDaoImpl implements UserDAO {
 
 	@Override
 	public User register(User user) {
-		// TODO 
+		// TODO
 		// persist a newly registered user to the database
 		System.out.println("-----------register does not work--------------");
 		return null;
@@ -39,11 +42,20 @@ public class UserDaoImpl implements UserDAO {
 	@Override
 	public boolean isUsernameUnique(String username) {
 		// TODO Auto-generated method stub
-		//return true if email is unique
-		// create jpql query to search for user with username passed in if result set is empty return true
+		// return true if email is unique
+		// create jpql query to search for user with username passed in if result set is
+		// empty return true
 		// else return false
+		boolean ans = false;
+		String jpql = "select user.username from User user where user.username = :blah";
+			List<String> dbUsernames = em.createQuery(jpql, String.class).setParameter("blah", username).getResultList();
+
+			if (dbUsernames.size() < 1) {
+				ans = true;
+			}
+
 		System.out.println("-----------isUsernameUnique does not work--------------");
-		return false;
+		return ans;
 	}
 
 	@Override
@@ -51,12 +63,21 @@ public class UserDaoImpl implements UserDAO {
 		// TODO Auto-generated method stub
 		/*
 		 * return true if email is unique 
-		 * create jpql query to search for user with email passed in if result set is empty return true
-		 * else return false
+		 * create jpql query to search for user with
+		 * email passed in if result set is empty return true else return false
 		 */
+
+		boolean ans = false;
+		String jpql = "select user.email from User user where user.email = :blah";
+		
+		List<String> dbEmails = em.createQuery(jpql, String.class).setParameter("blah", email).getResultList();
+		
+		if (dbEmails.size() < 1) {
+			ans = true;
+		}
+
 		System.out.println("-----------isEmailUnique does not work--------------");
-		return false;
+		return ans;
 	}
-	
 
 }
