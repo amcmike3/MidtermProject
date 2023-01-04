bdc3bfe8397213d117e076fe15b4fde17d75b1ed
Amcmike3 <amcmichael3@gmail.com>
2023-01-04 15:31:04 -0700

    add spring annotations to dao interface implementations for each entity

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/CompanyImageDAOImpl.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/CompanyImageDAOImpl.java
@@ -1,5 +1,17 @@
 package com.skilldistillery.jobsearch.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class CompanyImageDAOImpl implements CompanyImageDAO {
 
+	@PersistenceContext
+	private EntityManager em;
+	
+	
 }
