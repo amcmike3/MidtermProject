69c445c7a6a2262b922a04d4605e8bd4795161fd
SandraLeAnn <sharpnw21@gmail.com>
2023-01-04 11:09:18 -0700

    added to controller

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/UserController.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.jobsearch.controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +11,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.jobsearch.data.UserDAO;
+import com.skilldistillery.jobsearch.entities.Company;
+import com.skilldistillery.jobsearch.entities.Job;
 import com.skilldistillery.jobsearch.entities.User;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserDAO dao;
-	
-	@RequestMapping(path = {"/", "home"})
+
+	@RequestMapping(path = { "/", "home" })
 	public String home(Model model) {
-		
+
 		model.addAttribute("SMOKETEST", dao.findById(1));
 		return "home";
 	}
-	
+
 	@RequestMapping("login")
-	public String sendToLoginPage() {	
+	public String sendToLoginPage() {
 		return "login";
 	}
-	
+
 	@RequestMapping(path = "loggingIn", method = RequestMethod.POST)
-	public String login(String username, String password, HttpSession session) {	
+	public String login(String username, String password, HttpSession session) {
 		String ans = "";
 		User user = dao.login(username, password);
 		if (user == null) {
@@ -39,37 +43,38 @@ public class UserController {
 			session.setAttribute("user", user);
 			ans = "home";
 		}
-		
+
 		return ans;
 	}
-	
+
 	@RequestMapping(path = "loggingOut.do")
 	public String logout(HttpSession session) {
 		session.removeAttribute("user");
 		return "redirect:home";
 	}
-	
+
 	@RequestMapping("register")
-	public String sendToRegisterPage() {	
+	public String sendToRegisterPage() {
 		return "register";
 	}
 
 	@RequestMapping("registering")
-	public String register(User user, Model model, HttpSession session) {	
+	public String register(User user, Model model, HttpSession session) {
 		String ans = "";
-		//if username is unique and email is unique then register user otherwise 
-		//refresh register page with form pre-filled except for respective not unique input
-		if ( ! dao.isEmailUnique(user.getEmail())) {
-			model.addAttribute("notUniqueEmail", true);			
-			model.addAttribute("user", user);			
+		// if username is unique and email is unique then register user otherwise
+		// refresh register page with form pre-filled except for respective not unique
+		// input
+		if (!dao.isEmailUnique(user.getEmail())) {
+			model.addAttribute("notUniqueEmail", true);
+			model.addAttribute("user", user);
 			ans = "register";
-			
-		} else if( ! dao.isUsernameUnique(user.getUsername())) {
+
+		} else if (!dao.isUsernameUnique(user.getUsername())) {
 			model.addAttribute("notUniqueUsername", true);
 			model.addAttribute("user", user);
 			ans = "register";
 		} else {
-			User daoUser = dao.register(user);	
+			User daoUser = dao.register(user);
 			if (daoUser != null) {
 				session.setAttribute("user", daoUser);
 				ans = "home";
@@ -77,9 +82,54 @@ public class UserController {
 				ans = "register";
 			}
 		}
-		
+
 		return ans;
 	}
-	
-	
+
+	@RequestMapping("getUser.do")
+	public String getUsersByKeyword(String username, Model model) {
+		List<User> users = dao.findUsers(username);
+		model.addAttribute("userList", users);
+		return "results";
+
+	}
+
+	@RequestMapping("getCompany.do")
+	public String getCompanyByKeyword(String name, Model model) {
+		List<Company> company = dao.findCompanies(name);
+		model.addAttribute("companyList", company);
+		return "results";
+
+	}
+
+	@RequestMapping("getJob.do")
+	public String getJobByKeyword(String title, Model model) {
+		List<Job> job = dao.findJobs(title);
+		model.addAttribute("jobList", job);
+		return "results";
+
+	}
+
+	@RequestMapping("allUser.do")
+	public String getAllUsers(Model model) {
+		List<User> users = dao.findAllUsers();
+		model.addAttribute("userList", users);
+		return "results";
+
+	}
+
+	@RequestMapping("allCompanies.do")
+	public String getAllCompanies(Model model) {
+		List<Company> company = dao.findAllCompanies();
+		model.addAttribute("companyList", company);
+		return "results";
+	}
+
+	@RequestMapping("allJobs.do")
+	public String getAllJobs(Model model) {
+		List<Job> job = dao.findAllJobs();
+		model.addAttribute("jobList", job);
+		return "results";
+
+	}
 }
