a309dc4b66c8a03b7f0484d2de1b821b9fba8ee9
Michael Harrington <mdharr7@gmail.com>
2023-01-05 06:16:45 +0800

    Add method in UserDaoImpl for createUserReview

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/UserDaoImpl.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/data/UserDaoImpl.java
@@ -147,8 +147,26 @@ public class UserDaoImpl implements UserDAO {
 	}
 
 	@Override
-	public CompanyReview createUserReview(CompanyReview review) {
-		CompanyReview companyReview = new CompanyReview();
-		return review;
+	public CompanyReview createUserReview(CompanyReview companyReview){
+	    CompanyReview review = null;
+	    int userId = companyReview.getUser().getId();
+	    User user = findById(userId);
+	    int companyId = companyReview.getCompany().getId();
+	    Company company = findCompanyById(companyId);
+	    
+	    if(user != null && company != null){
+	        review = new CompanyReview();
+	        review.setContent(companyReview.getContent());
+	        review.setReviewDate(companyReview.getReviewDate());
+	        review.setUser(user);
+	        review.setRecommendation(companyReview.isRecommendation());
+	        review.setRating(companyReview.getRating());
+	        review.setPros(companyReview.getPros());
+	        review.setCons(companyReview.getCons());
+	        review.setTitle(companyReview.getTitle());
+	        review.setAdvice(companyReview.getAdvice());
+	        review.setCompany(company);
+	    }
+	    return review;
 	}
 }
