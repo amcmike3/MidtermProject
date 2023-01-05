6d751394703e62025e36b85c97a851807482f175
Amcmike3 <amcmichael3@gmail.com>
2023-01-05 14:43:15 -0700

    stub out all links from jobBio.jsp and handle it in controller also add all pages associated with stubbed out links

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/InterviewController.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/InterviewController.java
@@ -2,12 +2,34 @@ package com.skilldistillery.jobsearch.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.jobsearch.data.InterviewDAO;
 
 @Controller
 public class InterviewController {
-	
+
 	@Autowired
 	private InterviewDAO dao;
+
+	@RequestMapping("interviewBio")
+	public ModelAndView interviewBio(Integer jobId) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("interviewBio");
+		return mv;
+	}
+
+	@RequestMapping("allInterviews")
+	public ModelAndView allInterviews(Integer jobId) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("allInterviews");
+		return mv;
+	}
+	
+	@RequestMapping("createInterview")
+	public String createInterview(Integer jobId) {
+		
+		return "createInterview";
+	}
 }
