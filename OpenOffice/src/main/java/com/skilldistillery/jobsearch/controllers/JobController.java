f8218aaa22ec35caa9c46ff5b8630d233d6d8d34
SandraLeAnn <sharpnw21@gmail.com>
2023-01-08 21:01:13 -0700

    edit

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/JobController.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/JobController.java
@@ -50,30 +50,21 @@ public class JobController {
 		model.addAttribute("job", job);
 		return "jobBio";
 	}
-
+    //API method that is handling the createJob.do call from the front end jsp.
 	@PostMapping("createJob.do")
+	//
 	public ModelAndView jobTypeCreatedForInterview(Job job, Integer companyId, Integer industryId) {
 		ModelAndView mv = new ModelAndView();
 		Industry industry = inDao.findById(industryId);
 		Company company = compDao.findCompanyById(companyId);
-		// find the company that the job is created for
-		// get the list of jobs for that company
-//		List<Job> jobs = dao.findAllJobsWithinASpecificCompany(companyId);
 		job.setCompany(company);
-		// once the back end has the industry the user picked, set it to the new job
+	
 		job.setIndustry(industry);
 		
-		
-		
 		Job newJob = dao.createJob(job);
-		// actually create the job with the info from the form and the company and list
-		// set
-		// find the industry that the user picked in the drop down to assign it to the
-		// job(join column)
-		industry = inDao.findById(industryId);
-		// add the job to the pre existing list of jobs that a company has
+
 		company.addJob(newJob);
-		// set the company to the job that was created.. because a Job HAS A Company.
+
 		newJob.setCompany(company);
 
 		mv.addObject("jobId", newJob.getId());
