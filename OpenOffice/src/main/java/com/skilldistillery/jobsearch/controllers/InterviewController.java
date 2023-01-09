3a58d16011594724d07820e5264cf0b590b10bf7
Amcmike3 <amcmichael3@gmail.com>
2023-01-09 11:21:36 -0700

    complete crud for interviews

--- OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/InterviewController.java
+++ OpenOffice/src/main/java/com/skilldistillery/jobsearch/controllers/InterviewController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.jobsearch.data.InterviewDAO;
@@ -66,6 +67,7 @@ public class InterviewController {
 	
 	@RequestMapping("updatingInterview")
 	public String updatingInterview(Integer companyId, Interview interview, Model model) {
+		System.out.println("----------------------------" + interview);
 		dao.updateInterview(interview.getId(), interview);
 		model.addAttribute("company", dao.findCompanyById(companyId));
 		
